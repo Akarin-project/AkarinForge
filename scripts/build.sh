34e84529630806ef09ead4af54f3229abff088e4
retroheim <60027238+retroheim@users.noreply.github.com>
2020-02-16 19:52:03 -0500

    r

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ if [ "$2" == "--setup" ] || [ "$3" == "--setup" ] || [ "$4" == "--setup" ]; then
 		cd "$forgebasedir"
 		\cp -rf "$basedir/jsons" "$forgebasedir/"
 		echo "[Akarin] Setup forge.."
-		./gradlew setupForge
+		./gradlew setup
 
 		echo "[Akarin] Touch workspace.."
 		#\cp -rf "$forgebasedir/projects/Forge/.settings" "$basedir/"
