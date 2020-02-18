0521c5b73bcbe3403c7de0f0a22d1f1a8c926b2a
retroheim <60027238+retroheim@users.noreply.github.com>
2020-02-17 23:23:20 -0500

    Delete maven.sh

--- scripts/maven.sh
+++
@@ -1,6 +0,0 @@
- sudo wget http://mirror.its.dal.ca/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
- tar xzvf apache-maven-3.6.3-bin.tar.gz 
- export M2_HOME=/apache-maven-3.6.3 
- export M2=$M2_HOME/bin 
-#
-#
